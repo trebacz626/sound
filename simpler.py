666a6bf4fc562458e47ef2406ade30e516d35760
Meeso1 <kubaknyspel@gmail.com>
2023-03-25 16:18:59 +0100

    Fundamental freq + LSTER (both don't fully work yet)

--- simpler.py
+++ simpler.py
@@ -1,8 +1,5 @@
 import sys
-
-import numpy as np
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.patches import Rectangle
@@ -11,19 +8,22 @@ from scipy.io import wavfile
 from sound_params import *
 
 
-framerate, sound = wavfile.read('./sounds/me.wav')
-# sound = sound[:, 1].astype(np.float64)
-nframes = len(sound)
-duration = nframes / framerate
+framerate, sound = wavfile.read('C:\\Users\\kubak\\Documents\\Studia\\6 semestr\\AIPD\\Nagrania\\2_05.wav')
+n_frames = len(sound)
+duration = n_frames / framerate
 print(framerate)
-times = np.array([i/framerate for i in range(nframes)])
-fram_size_ms = 40
-frame_size = framerate/(1000/fram_size_ms)
+times = np.array([i / framerate for i in range(n_frames)])
+frame_size_ms = 40
+frame_size = framerate/(1000 / frame_size_ms)
 
 calculated_volume = efficient_volume(sound, frame_size)
 calculated_zcr = efficient_zcr(sound, frame_size)
 times_window = efficient_sample_time(times, frame_size)
+seconds_window = efficient_sample_time(times_window, 1000 // frame_size_ms)
 silence = times_window[(calculated_zcr > 0.07) & (calculated_volume < 200)]
+
+fundamental_freq = efficient_fundamental_frequency(sound, 10, frame_size)
+calculated_lster = lster(sound, frame_size, framerate)
 
 
 class MainWindow(QMainWindow):
@@ -53,20 +53,31 @@ class MainWindow(QMainWindow):
         vbox1.addWidget(self.canvas2)
         vbox1.addWidget(self.canvas3)
 
-
         self.reset_button = QPushButton('Reset')
         self.reset_button.clicked.connect(self.reset_plots)
         hbox.addWidget(self.reset_button)
 
         self.span_selector = SpanSelector(self.ax1, self.onselect, 'horizontal', useblit=True)
 
+        self.vdr_label = QLabel(f"VDR:{vdr(sound, frame_size)}")
+        vbox2.addWidget(self.vdr_label)
+
+        self.figure4 = Figure(figsize=(5, 4), dpi=100)
+        self.canvas4 = FigureCanvas(self.figure4)
+        self.ax4 = self.figure4.add_subplot(111)
+        self.draw_plot_fundamental_frequency()
+        vbox2.addWidget(self.canvas4)
+
+        self.figure5 = Figure(figsize=(5, 4), dpi=100)
+        self.canvas5 = FigureCanvas(self.figure5)
+        self.ax5 = self.figure5.add_subplot(111)
+        self.draw_plot_lster()
+        vbox2.addWidget(self.canvas5)
+
         hbox.addLayout(vbox1)
+        hbox.addLayout(vbox2)
         hbox.addStretch(1)
 
-        self.vdr_label = QLabel(f"VDR:{vdr(sound, frame_size)}")
-        # text_field = QLineEdit()
-        vbox2.addWidget(self.vdr_label)
-        hbox.addLayout(vbox2)
         central_widget = QWidget()
         central_widget.setLayout(hbox)
         self.setCentralWidget(central_widget)
@@ -79,14 +90,15 @@ class MainWindow(QMainWindow):
         self.draw_plot_sound(x_min, x_max)
         self.draw_plot_volume(xmin, xmax)
         self.draw_plot_zcr(xmin, xmax)
-
+        self.draw_plot_fundamental_frequency(xmin, xmax)
+        self.draw_plot_lster(xmin, xmax)
 
     def draw_plot_sound(self, x_min=0, x_max=sys.maxsize):
         self.ax1.clear()
         selected_times = times[x_min:x_max]
         selected_sounds = sound[x_min:x_max]
         for s in silence[(silence >= selected_times[0]) & (silence <= selected_times[-1])]:
-            self.ax1.add_patch(Rectangle((s-fram_size_ms/1000, -10000), fram_size_ms/1000, 20000, facecolor="red"))
+            self.ax1.add_patch(Rectangle((s - frame_size_ms / 1000, -10000), frame_size_ms / 1000, 20000, facecolor="red"))
         self.ax1.plot(selected_times, sound[x_min:x_max])
         self.ax1.set_title('Sound Wave')
         self.ax1.set_ylim((selected_sounds.min()*0.9, selected_sounds.max()*1.1))
@@ -111,6 +123,26 @@ class MainWindow(QMainWindow):
         self.ax3.plot(times_window[x_min:x_max], calculated_zcr[x_min:x_max])
         self.ax3.set_title('Zero Crossing Rate')
         self.canvas3.draw()
+
+    def draw_plot_fundamental_frequency(self, x_min=0, x_max=sys.maxsize):
+        self.ax4.clear()
+
+        min_idx = np.searchsorted(times_window, x_min, side="left")
+        max_idx = np.searchsorted(times_window, x_max, side="right")
+
+        self.ax4.plot(times_window[min_idx:max_idx], fundamental_freq[min_idx:max_idx])
+        self.ax4.set_title('Fundamental Frequency')
+        self.canvas4.draw()
+
+    def draw_plot_lster(self, x_min=0, x_max=sys.maxsize):
+        self.ax5.clear()
+
+        min_idx = np.searchsorted(seconds_window, x_min, side="left")
+        max_idx = np.searchsorted(seconds_window, x_max, side="right")
+
+        self.ax5.plot(seconds_window[min_idx:max_idx], calculated_lster[min_idx:max_idx])
+        self.ax5.set_title('LSTER')
+        self.canvas5.draw()
 
     def reset_plots(self):
         self.draw_plot_sound()
--- sound_params.py
+++ sound_params.py
@@ -12,20 +12,32 @@ def ste(sound, window_size=DEFAULT_WINDOW_SIZE):
 
 
 def volume(sound, window_size=DEFAULT_WINDOW_SIZE):
-    # print(ste(sound,window_size))
-    return np.sqrt(ste(sound,window_size))
+    return np.sqrt(ste(sound, window_size))
 
 
 def zcr(sound, window_size=DEFAULT_WINDOW_SIZE):
     slided = sliding_window_view(sound, window_size-1)
     return 1/window_size*0.5*np.sum(np.abs(np.sign(slided[1:]) - np.sign(slided[:-1])), axis=1)
 
+
 def rn(sound, l, window_size=DEFAULT_WINDOW_SIZE):
     pass
 
 
+def lster(sound, window_size=DEFAULT_WINDOW_SIZE, big_window_size=DEFAULT_WINDOW_SIZE*25):
+    calculated_ste = efficient_ste(sound, window_size)
+    num_windows = int(big_window_size // window_size)
+    lster = []
+    for i in range(num_windows, len(calculated_ste), num_windows//2):
+        chunk = np.array(calculated_ste[i-num_windows:i])
+        avg = chunk.mean()
+        v = np.sign(chunk - 0.5*avg) + 1
+        lster.append(v.sum() * 0.5 / num_windows)
 
-#efficient
+    return np.array(lster)
+
+
+# efficient
 
 @jit(nopython=True)
 def efficient_ste(sound, window_size=DEFAULT_WINDOW_SIZE):
@@ -36,6 +48,7 @@ def efficient_ste(sound, window_size=DEFAULT_WINDOW_SIZE):
     for i in range(window_size, l, distance):
         ste.append(sound_squared[i-window_size:i].mean())
     return np.array(ste)
+
 
 @jit(nopython=True)
 def efficient_volume(sound, window_size=DEFAULT_WINDOW_SIZE):
@@ -70,13 +83,9 @@ def efficient_sample_time(times, window_size=DEFAULT_WINDOW_SIZE):
     return np.array([times[i-distance] for i in range(window_size, l, distance)])
 
 
-
 ###CLIP LEVEL###
-
 @jit(nopython=True)
 def vdr(sound, window_size=DEFAULT_WINDOW_SIZE):
     vol = efficient_volume(sound, window_size)
 
     return (vol.max()-vol.min())/vol.min()
-
-
